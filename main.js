182ed34ff4eb4712f3788f9ceeda462fe8386e65
vladimirk <zukikara@gmail.com>
2019-05-31 11:09:58 +0200

    Small fix

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+"use strict";
+
 const canvas = document.querySelector('#draw');
 const ctx = canvas.getContext('2d');
 
@@ -14,9 +16,8 @@ let lastY = 0;
 let hue = 0;
 let direction = true;
 
-function draw(e) {
-  if (!isDrawing) return; // stop the fn from running when they are not moused down
-  console.log(e);
+const draw = (e) => {
+  if (!isDrawing) return; // stop the fn from running when they are not moused down  
   ctx.strokeStyle = `hsl(${hue}, 100%, 50%)`;
   ctx.beginPath();
   // start from
